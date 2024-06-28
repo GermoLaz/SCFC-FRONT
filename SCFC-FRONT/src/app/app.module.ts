1cdc4f589d47a2f0047572421f1b2fcc4b770dbf
Enzo <menna.enzo@hotmail.com>
2024-06-28 19:27:19 -0300

    fix main page

--- SCFC-FRONT/src/app/app.module.ts
+++ SCFC-FRONT/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { PageModule } from './pages/pages.module';
+import { HttpClientModule } from '@angular/common/http';
 
 
 @NgModule({
@@ -16,6 +17,7 @@ import { PageModule } from './pages/pages.module';
     AppRoutingModule,
     SharedModule,
     PageModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
